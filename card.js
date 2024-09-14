fe3fced2291e2e2c765b6a41783df1a62c73fa73
O'Brien Otieno <obrienotieno@OBrien-MacBook-Air.local>
2024-09-14 17:59:44 +0300

    modify card

--- card.js
+++ card.js
@@ -1,139 +1,137 @@
 #!/usr/bin/env node
 
-'use strict'
+'use strict';
 
-// const boxen = require("boxen");
-import boxen from "boxen";
-// const chalk = require("chalk");
-import chalk from "chalk";
-// const inquirer = require("inquirer");
-import inquirer from "inquirer";
-// const clear = require("clear");
-import clear from "clear";
-// const open = require("open");
-import open from "open";
-// const fs = require('fs');
-import fs from "fs"
-// const request = require('request');
-import request from "request";
-// const path = require('path');
-import path from "path";
-// const ora = require('ora');
-import ora from "ora";
-// const cliSpinners = require('cli-spinners');
-import cliSpinners from "cli-spinners";
+// Importing modules
+import boxen from 'boxen';
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import clear from 'clear';
+import open from 'open';
+import fs from 'fs';
+import request from 'request';
+import path from 'path';
+import ora from 'ora';
+import cliSpinners from 'cli-spinners';
 
+// Clear console
 clear();
 
+// Create inquirer prompt
 const prompt = inquirer.createPromptModule();
 
+// Define questions for user interaction
 const questions = [
     {
-        type: "list",
-        name: "action",
-        message: "What you want to do?",
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
         choices: [
             {
-                name: `Send me an ${chalk.green.bold("email")}?`,
+                name: `Send me an ${chalk.green.bold('email')}?`,
                 value: () => {
-                    open("mailto:obrienotieno@icloud.com");
-                    console.log("\nAll Set!, Waiting to hear from you!\n");
+                    open('mailto:obrienotieno@icloud.com');
+                    console.log('\nAll set! Waiting to hear from you!\n');
                 }
             },
             {
-                name: `Download my ${chalk.magentaBright.bold("Resume")}?`,
+                name: `Download my ${chalk.magentaBright.bold('Profile')}?`,
                 value: () => {
-                    // cliSpinners.dots;
                     const loader = ora({
-                        text: ' Downloading Profile',
+                        text: 'Downloading Profile...',
                         spinner: cliSpinners.material,
                     }).start();
-                    let pipe = request('https://regent-ai.com/brianotieno/profile').pipe(fs.createWriteStream('./brianotieno-profile.html'));
-                    pipe.on("finish", function () {
-                        let downloadPath = path.join(process.cwd(), 'brianotieno-profile.html')
-                        console.log(`\nProfile Downloaded at ${downloadPath} \n`);
-                        open(downloadPath)
-                        loader.stop();
-                    });
+                    
+                    const filePath = path.join(process.cwd(), 'brianotieno-profile.html');
+                    request('https://nairobi-quantum-fusion.com/brianotieno')
+                        .pipe(fs.createWriteStream(filePath))
+                        .on('finish', () => {
+                            console.log(`\nProfile downloaded at ${filePath}\n`);
+                            open(filePath);
+                            loader.stop();
+                        })
+                        .on('error', (err) => {
+                            console.error('Error downloading the profile:', err);
+                            loader.stop();
+                        });
                 }
             },
             {
-                name: `Schedule a 30 minute ${chalk.redBright.bold("Meeting")}?`,
+                name: `Schedule a 30-minute ${chalk.redBright.bold('Meeting')}?`,
                 value: () => {
                     open('https://calendly.com/brianotieno/30min');
-                    console.log("\n See you soon! \n");
+                    console.log('\nSee you soon!\n');
                 }
             },
             {
-                name: "Just quit.",
+                name: 'Just quit.',
                 value: () => {
-                    console.log("Adios. Hasta la vista.\n");
+                    console.log('Adios. Hasta la vista.\n');
                 }
             }
         ]
     }
 ];
 
+// Define user profile data
 const data = {
-    name: chalk.bold.green("             Brian Otieno"),
-    handle: chalk.white("@BrianOtieno"),
-    work: `${chalk.white("Chief Technology Officer at")} ${chalk
-        .hex("#2b82b2")
-        .bold("AI Regent")}`,
-    twitter: chalk.gray("https://twitter.com/") + chalk.cyan("OBrienOtieno"),
-    github: chalk.gray("https://github.com/") + chalk.green("BrianOtieno"),
-    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("obrienotieno"),
-    web: chalk.cyan("https://regent-ai.com"),
-    npx: chalk.red("npx") + " " + chalk.white("brianotieno"),
+    name: chalk.bold.green('Brian Otieno'),
+    handle: chalk.white('@BrianOtieno'),
+    work: `${chalk.white('Founder & Quantum Scientist at')} ${chalk.hex('#2b82b2').bold('NQF')}`,
+    twitter: chalk.gray('https://twitter.com/') + chalk.cyan('OBrienOtieno'),
+    github: chalk.gray('https://github.com/') + chalk.green('BrianOtieno'),
+    linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('obrienotieno'),
+    web: chalk.cyan('https://nairobi-quantum-fusion.com'),
+    npx: chalk.red('$ npx') + ' ' + chalk.magenta('brianotieno'),
 
-    labelWork: chalk.white.bold("       Work:"),
-    labelTwitter: chalk.white.bold("    Twitter:"),
-    labelGitHub: chalk.white.bold("     GitHub:"),
-    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
-    labelWeb: chalk.white.bold("        Web:"),
-    labelCard: chalk.white.bold("       Card:")
+    labelWork: chalk.white.bold('       Work:'),
+    labelTwitter: chalk.white.bold('    Twitter:'),
+    labelGitHub: chalk.white.bold('     GitHub:'),
+    labelLinkedIn: chalk.white.bold('   LinkedIn:'),
+    labelWeb: chalk.white.bold('        Web:'),
+    labelCard: chalk.white.bold('       Card:')
 };
 
-const me = boxen(
+// Generate and display user profile
+const profile = boxen(
     [
         `${data.name}`,
-        ``,
+        '',
         `${data.labelWork}  ${data.work}`,
-        ``,
+        '',
         `${data.labelTwitter}  ${data.twitter}`,
         `${data.labelGitHub}  ${data.github}`,
         `${data.labelLinkedIn}  ${data.linkedin}`,
         `${data.labelWeb}  ${data.web}`,
-        ``,
+        '',
         `${data.labelCard}  ${data.npx}`,
-        ``,
-        `${chalk.italic(
-            "I am open to new collaborations in tech and innovation,"
-        )}`,
-        `${chalk.italic("my inbox is always open. Whether you have a")}`,
-        `${chalk.italic(
-            "question or just want to say hi, I will try "
-        )}`,
-        `${chalk.italic(
-            "my best to get back to you!"
-        )}`
-    ].join("\n"),
+        '',
+        `${chalk.italic('I am open to collaborations in quantum research & innovation,')}`,
+        `${chalk.italic('my inbox is always open. Whether you have a')}`,
+        `${chalk.italic('question or just want to say hi, I will try my best')}`,
+        `${chalk.italic('to get back to you!')}`
+    ].join('\n'),
     {
         margin: 1,
         float: 'center',
         padding: 1,
-        borderStyle: "single",
-        borderColor: "green"
+        borderStyle: 'single',
+        borderColor: 'green'
     }
 );
 
-console.log(me);
+console.log(profile);
+
+// Display tip for interactive elements
 const tip = [
-    `Tip: Try ${chalk.cyanBright.bold(
-        "cmd/ctrl + click"
-    )} on the links above`,
-    '',
-].join("\n");
+    `Tip: Try ${chalk.cyanBright.bold('cmd/ctrl + click')} on the links above`,
+    ''
+].join('\n');
 console.log(tip);
 
-prompt(questions).then(answer => answer.action());
+// Prompt user for action
+prompt(questions).then(answer => {
+    if (typeof answer.action === 'function') {
+        answer.action();
+    }
+});
